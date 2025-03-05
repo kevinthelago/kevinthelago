815d3fc7054da0c64138059418a602690312891b
Kevin Lago <kevinthelago@gmail.com>
2025-03-05 18:11:52 -0500

    print line

--- .github/workflows/topics.py
+++ .github/workflows/topics.py
@@ -7,14 +7,13 @@ print(os.listdir(ROOT_DIR))
 README = ROOT_DIR + "/README.md"
 print(f"README: {README}")
 
-
 with open(README, 'w') as readme:
     readme.write("test write")
+    readme.close()
 
+with open(README, 'r') as readme:
     for line in readme:
         print(line)
-        
-    readme.close()
 
 if __name__ == '__main__':
     print("requests test")
