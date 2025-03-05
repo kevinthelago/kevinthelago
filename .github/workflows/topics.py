46d82d3ffe87235ffc0327d0812f808be52e4772
Kevin Lago <kevinthelago@gmail.com>
2025-03-05 18:01:33 -0500

    File Write Test

--- .github/workflows/topics.py
+++ .github/workflows/topics.py
@@ -1,4 +1,13 @@
+import os
 import requests
+
+ROOT_DIR = os.getcws()
+print(f"Root Directory: {ROOT_DIR}")
+README = ROOT_DIR + "/readme.md"
+
+
+with open(README, w) as readme:
+    readme.write("test write")
 
 if __name__ == '__main__':
     print("requests test")
